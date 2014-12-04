059e03761bc91a9a902338bfb7bf720c41c5704c
Jiri Kraus <jiri.kraus@gmail.com>
2014-12-04 02:13:15 +0100

    include hlavicka vsude

--- src/kontakt.php
+++ src/kontakt.php
@@ -1,54 +1,6 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>kontakt</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300&subset=cyrillic-ext,latin' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="css/reset.css"/>
-        <link rel="stylesheet" href="css/style.css"/>
-        <!--<link rel="stylesheet" href="css/style01.css"/>-->
-        <!--<link rel="stylesheet" href="css/style02.css"/>-->
-        <!--<link rel="stylesheet" href="css/style03.css"/>-->
-    </head>
-    <body>
-        <div id="container">
-
-
-            <div id="page-header">
-
-                <img src="images/haeder01 copy.jpg" alt=""/>
-            </div>    
-            <div id="top-menu">
-                <a href="index.html">DOMŮ</a>
-                <a href="koucovani.html">KOUČOVÁNÍ</a>
-                <a href="Pro firmy.html">PRO FIRMY </a>
-                <a href="Cenik.html">CENÍK</a>
-                <a href="kontakt.html">KONTAKT</a>
-                <a href="faq.html">FAQ</a>
-            </div>
-
-            <div id="page-content">
-
-                <!--<div id="inconstruction">Na stránce se pracuje</div>-->
-
-                <div id="motto" class="section-motto">
-
-                    <div>
-                        <p>
-                            "Koučování je jako jízda kočárem, místy příjemná, místy může být nepohodlná. Někdy slunce krásně 
-
-                            svítí, nebe je modré, jindy se na cestě mohou vyskytnout výmoly, někdy udivíte nečekané dech 
-
-                            beroucí scenérie a příroda Vám ukáže všechny své poklady. Vždy si ale můžete říci, že to stálo za to."          </p>
-
-                    </div>
-                </div>
+<?php
+    include './-hlavicka.php';
+?>
 
                 <div id="kontakt" class="section-full">
                     <div class="sub-section-bgr-text">
--- src/koucovani.php
+++ src/koucovani.php
@@ -1,54 +1,6 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>CoachingExcellence</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300&subset=cyrillic-ext,latin' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="css/reset.css"/>
-        <link rel="stylesheet" href="css/style.css"/>
-        <!--<link rel="stylesheet" href="css/style01.css"/>-->
-        <!--<link rel="stylesheet" href="css/style02.css"/>-->
-        <!--<link rel="stylesheet" href="css/style03.css"/>-->
-    </head>
-    <body>
-        <div id="container">
-
-
-            <div id="page-header">
-
-                <img src="images/haeder01 copy.jpg" alt=""/>
-            </div>    
-            <div id="top-menu">
-                <a href="index.html">DOMŮ</a>
-                <a href="koucovani.html#koucovani">KOUČOVÁNÍ</a>
-                <a href="Pro firmy.html#profirmy">PRO FIRMY</a>
-                <a href="Cenik.html#cenik">CENÍK</a>
-                <a href="kontakt.html#kontakt">KONTAKT</a>
-                <a href="faq.html#faq">FAQ</a>
-            </div>
-
-            <div id="page-content">
-
-                <!--<div id="inconstruction">Na stránce se pracuje</div>-->
-
-                <div id="motto" class="section-motto">
-
-                    <div>
-                        <p>
-                            "Koučování je jako jízda kočárem, místy příjemná, místy může být nepohodlná. Někdy slunce krásně 
-
-                            svítí, nebe je modré, jindy se na cestě mohou vyskytnout výmoly, někdy udivíte nečekané dech 
-
-                            beroucí scenérie a příroda vám ukáže všechny své poklady. Vždy si ale můžete říci, že to stálo za to."          </p>
-
-                    </div>
-                </div>
+<?php
+    include './-hlavicka.php';
+?>
 
                 <div id="koucovani" class="section-full">
                     <div class="sub-section-bgr-text">
